17efc0a70d134c434aecc8fe29a303e387ec8415
git-ryou2405 <r2405.exe@gmail.com>
2020-08-24 02:12:55 +0900

    Reptiles Searchへ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   
   # ページごとにタイトルを返す
   def full_title(page_name = "") # メソッドと引数の定義
-    base_title = "Reptiles Farm" + " | " + "爬虫類を全国SHOPから一括検索！"
+    base_title = "Reptiles Search" + " | " + "爬虫類を全国SHOPから一括検索！"
   end
 end
